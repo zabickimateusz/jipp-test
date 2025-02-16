c73e40e91f0e6639e327e4d7d6660205a029b85b
Mateusz Żabicki <zabickimateusz64@gmail.com>
2025-02-16 11:18:26 +0100

    zadanie 1

--- JiPP/JiPP/Program.cs
+++ JiPP/JiPP/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //ZADANIE 1
+
+        int number = 77;                  
+        float money = 21.50f;            
+        string text = "Hello World";      
+        bool isLogged = true;            
+        char myChar = 'A';               
+        decimal price = 200.10m;         
+
+        Console.WriteLine($"number = {number}");
+        Console.WriteLine($"money = {money}");
+        Console.WriteLine($"text = {text}");
+        Console.WriteLine($"isLogged = {isLogged}");
+        Console.WriteLine($"myChar = {myChar}");
+        Console.WriteLine($"price = {price}");
+
+
+    }
+}
